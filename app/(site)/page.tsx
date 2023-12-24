8d3792a726b44dfa0b7081ca8bbc7db4308fe892
Phillip Phan <colPhilPhan@gmail.com>
2023-12-24 17:30:22 -0600

    Sidebar

---
+++ app/(site)/page.tsx
@@ -0,0 +1,9 @@
+// important to put in parenthesis so that it doesn't change route of app
+export default function Home() { 
+  return (
+    <div className="text-green-500">
+      Main Content
+    </div>
+
+  )
+}
