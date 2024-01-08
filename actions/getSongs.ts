13e7a8365333e53c0ba08205518e6d90692e7a27
Phillip Phan <colPhilPhan@gmail.com>
2024-01-08 17:02:20 -0600

    Favorites functionality works (Figure out why name of songs table is causing problems when it isn't named songs)

--- actions/getSongs.ts
+++ actions/getSongs.ts
@@ -1,22 +1,23 @@
-import { Song } from "@/types";
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
-import { cookies, headers } from "next/headers";
+import { cookies } from "next/headers";
+
+import { Song } from "@/types";
 
 const getSongs = async (): Promise<Song[]> => {
-    const supabase = createServerComponentClient({
-        cookies: cookies
-    });
+  const supabase = createServerComponentClient({
+    cookies: cookies
+  });
 
-    const { data, error } = await supabase
-        .from('Songs')
-        .select('*')
-        .order('created_at', { ascending: false});
+  const { data, error } = await supabase
+    .from('songs')
+    .select('*')
+    .order('created_at', { ascending: false })
 
-        if(error) {
-            console.log(error);
-        }
+  if (error) {
+    console.log(error.message);
+  }
 
-        return (data as any) || [];
- };
+  return (data as any) || [];
+};
 
- export default getSongs;
+export default getSongs;
